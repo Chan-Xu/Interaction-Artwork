2f74af1789c5a111c0245ab086ed79d5dceec6b2
U6233112 <u6233112@anu.edu.au>
2017-10-25 17:17:18 +1100

    basic

--- interaction-statement.md
+++ interaction-statement.md
@@ -1,3 +1,15 @@
 # Interaction Statement
 
 Write your interaction statement in this file (max 500 words).
+
+
+ tell a story：
+交代背景： 现在是个性化医疗时代，
+story：一个人有一天生病了， 去医院，什么病，需要什么药
+  选择不同颜色的不同数量配比的药丸
+  right--> 病好了
+  wrong--> die
+
+
+第二个人来了医院：
+  不同的人不同的处方：不同的药丸数量配比
--- sketch.js
+++ sketch.js
@@ -1,8 +1,133 @@
+var img1;
+var img2;
+
+var pills = [];
+var score;
+var playing; // aids with asychronous endGame() function
+
+function preload() {
+    img1 = loadImage("assets/patient.png");
+    img2 = loadImage("assets/logo.png");
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     // any additional setup code goes here
+    words = "This is a personalized medical age, for different people to implement different medical treatment. Personalized medicine, also termed precision medicine, is a medical procedure that separates patients into different groups—with medical decisions, practices, interventions and/or products being tailored to the individual patient based on their predicted response or risk of disease. "
+    score = 0;
+    speed = 1;
+    playing = true;
+
 }
 
 function draw() {
     // your "draw loop" code goes here
+    scene2();
+//    drawPixels();
+}
+
+function drawPixels() {
+	push();
+		textSize(12);
+		fill(255);
+		rect(mouseX-20, mouseY-12, 90, 15);
+		fill(0);
+		text("X: "+Math.round(mouseX)+", Y: "+Math.round(mouseY), mouseX-16, mouseY);
+	pop();
+}
+
+
+function scene1() {
+    background(0);
+    textFont("Times New Roman");
+    textSize(28);
+    fill(255);
+    text(words.substring(0, frameCount/2), width/4, height/4, width/2, height/2);
+}
+
+
+function scene2() {
+    noStroke();
+    fill(210, 190, 160);// floor
+    rect(0, height*4/5, width, height/5);
+    fill(140, 140, 140);// wall edge
+    rect(0, height*4/5, width, height/25);
+    fill(150, 210, 255);// wall
+    rect(0, 0, width, height*4/5);
+
+    fill(120, 90, 60);// door
+    rect(width/5, height/5, width/5, height*16/25);
+    fill(170, 120, 70);
+    rect(width/5+width/60, height/5+height/30, width/5-width/30, height*16/25-height/30);
+    fill(0);
+    ellipse(width/5+width/30, height*3/5, 20, 20);
+    textSize(32);
+    fill(255);
+    text("DOCTOR", width/4, height*2/5);
+
+//    image(img2, width/2, height/10, width/5, width/5);
+    image(img1, width/2, height/4, width/3, height*3/4);
+
+    /* dialogs*/
+    rect()
+
+}
+
+
+function scene3() {
+  background(0);
+
+  if (frameCount % 60 === 0)
+    if (random() > 0.6)
+      pills.push(new Pill(random(width / 2), (random() > 1)));
+
+  if (frameCount % 600 === 0)
+    speed++;
+
+  for (var i = pills.length - 1; i >= 0; i--) {
+
+    pills[i].update();
+    pills[i].draw();
+
+
+
+    if (pills[i].captured) {
+
+      pills.splice(i, 1);
+      score++;
+    }
+}
+
+/* draw score */
+  fill(255);
+  noStroke();
+  text(score, 10, 40);
+
+  if (!playing) {
+
+    fill(255);
+    noStroke();
+    textSize(60);
+    textAlign(CENTER);
+    text("Game Over!", width / 2, height / 2);
+//    textAlign(LEFT);
+//    textSize(30);
+  }
+}
+
+function mousePressed() {
+
+  for (var i = 0; i < pills.length; i++) {
+
+    pills[i].captured = pills[i].capturedBy(mouseX, mouseY);
+
+    if (pills[i].captured && pills[i].type)
+      endGame();
+  }
+}
+
+function endGame() {
+
+  playing = false;
+  noLoop();
 }
